08ec225dcbce26cc05919745024a22dce76aa9cb
David Raffensperger <d.raffensperger@gmail.com>
2013-06-02 17:31:04 -0400

    Added rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miletracker::Application.config.secret_token = '1844227d45cf18deffee509e73cd9b6e872c553ab4bb506817541bf6e3cb8839e518f0278bcccd10c89a4673fcb366fd31e0f43cfdbad6bb091d7d427b18ea77'
