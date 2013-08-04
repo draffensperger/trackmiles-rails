b808dd906fc0c51d9240b93d4d7ab72f7e98f889
David Raffensperger <contact@davidraff.com>
2013-08-03 22:11:46 -0400

    Went back to config SSLL enforcement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect_to_ssl
   before_filter :ensure_login  
-    
+  
+=begin   
+    before_filter :redirect_to_ssl
   def redirect_to_ssl
     #if Rails.env.production?
       redirect_to :protocol => "https://" unless (request.ssl?)
     #end
   end
+=end
   
   def ensure_login
     # fill in later  
