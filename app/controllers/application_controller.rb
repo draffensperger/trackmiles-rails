6dc4f062345386c3c589775eb731a2ddf0a8d65e
David Raffensperger <contact@davidraff.com>
2013-08-03 21:54:12 -0400

    Try SSL redirect for all environments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect_to_ssl if Rails.env.production?
+  before_filter :redirect_to_ssl
   before_filter :ensure_login  
     
   def redirect_to_ssl
+    #if Rails.env.production?
       redirect_to :protocol => "https://" unless (request.ssl?)
+    #end
   end
   
   def ensure_login
