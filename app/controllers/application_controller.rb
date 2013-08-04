627a5f0a8df0c89329e54f9b588493cf010cee0f
David Raffensperger <contact@davidraff.com>
2013-08-03 21:45:39 -0400

    Put back SSL setting, changed order of custom SSL redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :ensure_login
   before_filter :redirect_to_ssl if Rails.env.production?
+  before_filter :ensure_login  
     
   def redirect_to_ssl
       redirect_to :protocol => "https://" unless (request.ssl?)
