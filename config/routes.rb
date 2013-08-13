a70f20cba6eaef72ebade1248c00bd31500df878
David Raffensperger <contact@davidraff.com>
2013-08-12 21:00:04 -0400

    Added views for home links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Miletracker::Application.routes.draw do
   devise_for :users, 
-  	:controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
-  root :to => 'home#index'
+  	:controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   namespace :api do
     api_version(module: 'v1', header: {name: 'API-VERSION', value: 'v1'}, 
-                parameter: {name: "version", value: 'v1'}, 
+                parameter: {name: 'version', value: 'v1'}, 
                 path: {value: 'v1'}) do
       resources :locations, only: :none do
         collection do
@@ -15,4 +13,10 @@ Miletracker::Application.routes.draw do
       end            
     end
   end
+  
+  root :to => 'home#index'
+  get 'about' => 'home#about'
+  get 'privacy' => 'home#privacy'
+  get 'terms' => 'home#terms'
+  get 'apps' => 'home#apps'
 end
