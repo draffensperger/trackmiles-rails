20046d7a8d9314e6b20172db67e32741a9dd19b7
David Raffensperger <contact@davidraff.com>
2013-11-10 09:27:10 -0500

    Uncommented the trip distance calculation code

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,10 +3,10 @@ class TripsController < ApplicationController
     TripSeparator.new(current_user).calc_and_save_trips
     @trips = current_user.trips 
     @trips.each do |t|
-      #unless t.distance
-      #  t.calc_distance
-      #  t.save
-      #end
+      unless t.distance
+        t.calc_distance
+        t.save
+      end
     end
   end
 end
