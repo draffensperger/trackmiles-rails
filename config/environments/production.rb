61aad6ee11f765e781a91ce6f832471696942c07
David Raffensperger <contact@davidraff.com>
2013-11-15 08:42:25 -0500

    Set production log level to debug to understand slow login issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,5 +12,7 @@ Miletracker::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   
+  config.log_level = :debug
+  
   Rails.logger = Logger.new(STDOUT)
 end
