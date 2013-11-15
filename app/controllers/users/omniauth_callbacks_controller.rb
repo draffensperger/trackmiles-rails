99dd20ce062109545d3d197c4f54bef9d54e650e
David Raffensperger <contact@davidraff.com>
2013-11-14 22:06:02 -0500

    Modified login code to reduce queries

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,12 +9,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	    @user.google_auth_token = credentials[:token]
 	    @user.google_auth_refresh_token = credentials[:refresh_token]
 	    @user.google_auth_expires_at = Time.at(credentials[:expires_at])
-	    @user.remember_me = true	    
-	    @user.save	   
+	    @user.remember_me = true
+	    
+	    # I do the sign_in first as it seems to save the record anyway
+	    # which then makes the save not repeat a database query.
+	    sign_in @user	    
+	    @user.save
 
 	    if @user.persisted?
 	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-	      sign_in_and_redirect @user, :event => :authentication
+	      redirect_to after_sign_in_path_for(@user), :event => :authentication
 	    else
 	      session["devise.google_data"] = request.env["omniauth.auth"]
 	      redirect_to new_user_registration_url
