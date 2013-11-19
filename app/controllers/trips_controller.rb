29c3d276b5f21089fee1bd0f3591d61246edeca7
David Raffensperger <contact@davidraff.com>
2013-11-19 15:59:01 -0500

    Moved trip separation back to trips page controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,7 @@
 class TripsController < ApplicationController
-  def index    
+  def index 
+    TripSeparator.new(current_user).calc_and_save_trips
+    
     @trips = current_user.trips.where("start_place_id <> end_place_id")
       .order("start_time ASC")
     @trips.each do |t|
