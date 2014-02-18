917991c93a5b54d6041c6328464eb211b2716381
David Raffensperger <contact@davidraff.com>
2014-02-18 10:15:06 -0500

    Fixed user lookup method

--- app/models/trip_separator_worker.rb
+++ app/models/trip_separator_worker.rb
@@ -2,7 +2,7 @@ class TripSeparatorWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    user = Users.find_by_id user_id
+    user = User.find_by_id user_id
     if user
       TripSeparator.new(user).calc_and_save_trips
     end    
