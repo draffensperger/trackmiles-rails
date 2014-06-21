50df395a6cd263e17516d326897fed0c8653bff7
David Raffensperger <contact@davidraff.com>
2014-06-21 09:36:21 -0400

    Set debug log level for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,8 @@ Miletracker::Application.configure do
   #config.force_ssl = true
   config.force_ssl = false
   config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify  
+  config.active_support.deprecation = :notify
+  config.log_level = :debug
   
   Rails.logger = Logger.new(STDOUT)
 end
