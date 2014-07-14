d38cd1982391ceb0d1ee3fdf93c944bea367ea86
David Raffensperger <contact@davidraff.com>
2014-07-14 17:29:43 -0500

    Updated trips view to group by date and show start and end times

--- app/assets/javascripts/angular/trackmilesApp.js
+++ app/assets/javascripts/angular/trackmilesApp.js
@@ -1,8 +1,24 @@
 var app = angular.module('trackmilesApp', []);
 
-app.controller('TripsController', ['$scope', '$http', function($scope, $http) {
+app.controller('TripsController', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
     $http.get('/trips.json').success(function (data) {
-        $scope.trips = data['trips'];
+        trips = data.trips;
+        var trips_by_date = {};
+        var trip_dates = [];
+        for (var i = 0; i < trips.length; i++) {
+            var date = $filter('date')(trips[i]['start_time'], 'fullDate');
+            if (date in trips_by_date) {
+                trips_by_date[date].push(trips[i]);
+            } else {
+                trips_by_date[date] = [trips[i]];
+                trip_dates.push(date);
+            }
+        }
+        $scope.trip_dates = trip_dates;
+        $scope.places = data.places;
+        $scope.trips_by_date = trips_by_date;
     });
-    $scope.trips = [];
+    $scope.trip_dates = [];
+    $scope.trips_by_date = {};
+    $scope.places = {};
 }]);
