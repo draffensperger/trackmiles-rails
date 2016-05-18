22ac4819d6bf1d8deb12cd7cbfeba7aa0ade27d8
draffensperger <d.raffensperger@gmail.com>
2016-05-17 20:18:51 -0400

    Remove force_ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  force_ssl(if: :ssl_configured?)
   protect_from_forgery
   before_filter :ensure_login
 
@@ -13,9 +12,5 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource_or_scope)
     trips_path
-  end
-
-  def ssl_configured?
-    request.get? && !Rails.env.development? && !Rails.env.test?
   end
 end
